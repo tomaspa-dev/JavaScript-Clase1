4bccb1de16cbe6e3f2dbabdf4953e569d9cbad1d
tomaspa-dev <blord.dev@gmail.com>
2023-08-27 20:20:02 -0500

    more examples
    practice operator precedence

---
+++ assignments.js
@@ -0,0 +1,15 @@
+/* 1 - This is the first assigment
+LECTURE: Values and Variables */
+
+/* let country = "Peru";
+let continent = "America";
+let population = 30000000;
+
+console.log(
+  "Country: " +
+    country +
+    " and continent: " +
+    continent +
+    " and population: " +
+    population
+); */
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+/* 
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
@@ -13,13 +14,94 @@ console.log(firstName);
 let jinas_matilda = "JM";
 let $function = 27;
 
+//Variable name contentions
 let Person = "Jonas";
 let PI = 3.1415;
 
-let myFirstJob = "Programmer";
+let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
 let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof 23);
+// console.log(typeof "Jonas");
+// console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "coder";
+job = "teacher";
+
+lastName = "Schmedtman";
+console.log(lastName);
+
+
+//Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assigment operators
+let x = 10 + 5;
+x += 10; // x = x +10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //x = x + 1 = 101
+x--;
+x--; //x = 99
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
